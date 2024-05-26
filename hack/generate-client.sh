d6644b93f6a9817522d3dc8836e88ea26e0f79bc
Thomas Boerger <thomas@webhippie.de>
2024-05-27 00:33:50 +0200

    ci: use local generator cli for a release

--- hack/generate-client.sh
+++ hack/generate-client.sh
@@ -11,37 +11,55 @@ fi
 SPEC_VERSION="1.0.0-alpha1"
 SPEC_DOWNLOAD=${SPEC:-https://dl.gopad.eu/openapi/${SPEC_VERSION}.yml}
 
-if hash docker 2>/dev/null; then
-    COMMAND="docker"
-elif hash podman 2>/dev/null; then
-    COMMAND="postman"
-else
-    echo "missing container runtime"
-    exit 1
-fi
+pushd "${ROOT}" >/dev/null
+    echo "> install requirements"
+    npm install --ci
+popd >/dev/null
 
 echo "> deleteing apis and models"
 rm -rf api model
 
-echo "> regenerate apis and models"
-"${COMMAND}" run --rm \
-    -v "${ROOT}:/generate:Z" \
-    docker.io/openapitools/openapi-generator-cli:v7.6.0 \
-    generate \
-    -c /generate/openapi.yml \
-    -g typescript-axios \
-    --git-repo-id gopad-js \
-    --git-user-id gopad \
-    --http-user-agent "gopad-js/${SPEC_VERSION}" \
-    -i "${SPEC_DOWNLOAD}" \
-    -o /generate \
-    -t /generate/hack/openapi/templates \
-    --strict-spec true
+if hash openapi-generator-cli 2>/dev/null; then
+    export TS_POST_PROCESS_FILE="./node_modules/.bin/prettier -w"
 
-pushd "${ROOT}" >/dev/null
-  echo "> install requirements"
-  npm install --ci
+    openapi-generator-cli \
+        generate \
+        -c openapi.yml \
+        -g typescript-axios \
+        --git-repo-id gopad-js \
+        --git-user-id gopad \
+        --http-user-agent "gopad-js/${SPEC_VERSION}" \
+        -i "${SPEC_DOWNLOAD}" \
+        -o . \
+        -t hack/openapi/templates \
+        --strict-spec true
+else
+    if hash docker 2>/dev/null; then
+        COMMAND="docker"
+    elif hash podman 2>/dev/null; then
+        COMMAND="postman"
+    else
+        echo "missing container runtime"
+        exit 1
+    fi
 
-  echo "> fix code formatting"
-  npm run format
-popd >/dev/null
+    "${COMMAND}" run --rm \
+        -v "${ROOT}:/generate:Z" \
+        -w /generate \
+        docker.io/openapitools/openapi-generator-cli:v7.6.0 \
+        generate \
+        -c /generate/openapi.yml \
+        -g typescript-axios \
+        --git-repo-id gopad-js \
+        --git-user-id gopad \
+        --http-user-agent "gopad-js/${SPEC_VERSION}" \
+        -i "${SPEC_DOWNLOAD}" \
+        -o /generate \
+        -t /generate/hack/openapi/templates \
+        --strict-spec true
+
+    pushd "${ROOT}" >/dev/null
+        echo "> fix code formatting"
+        npm run format
+    popd >/dev/null
+fi
