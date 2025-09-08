20ea22fbe15731752356d4c1ca008bdbeaa81f32
tboerger <156964+tboerger@users.noreply.github.com>
2025-09-08 08:27:12 +0000

    feat: automated openapi client update

--- model/delete-group-from-user-request.ts
+++ model/delete-group-from-user-request.ts
@@ -12,16 +12,6 @@
  * Do not edit the class manually.
  */
 
-/**
- *
- * @export
- * @interface DeleteGroupFromUserRequest
- */
 export interface DeleteGroupFromUserRequest {
-  /**
-   *
-   * @type {string}
-   * @memberof DeleteGroupFromUserRequest
-   */
   user: string;
 }
